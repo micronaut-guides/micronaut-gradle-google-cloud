29944f81d26f866d231749cdebe7559f13399ee0
Iván López <lopezi@objectcomputing.com>
2019-11-18 14:11:48 +0100

    Use `@MicronautTest`

--- complete/src/test/java/example/micronaut/HelloControllerTest.java
+++ complete/src/test/java/example/micronaut/HelloControllerTest.java
@@ -1,46 +1,29 @@
 package example.micronaut;
 
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
-import io.micronaut.runtime.server.EmbeddedServer;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
+
+@MicronautTest
 public class HelloControllerTest {
 
-    private static EmbeddedServer server;
-    private static HttpClient client;
-
-    @BeforeClass
-    public static void setupServer() {
-        server = ApplicationContext.run(EmbeddedServer.class); 
-        client = server
-                .getApplicationContext()
-                .createBean(HttpClient.class, server.getURL());  
-    }
-
-    @AfterClass
-    public static void stopServer() {
-        if (server != null) {
-            server.stop();
-        }
-        if (client != null) {
-            client.stop();
-        }
-    }
+    @Inject
+    @Client("/")
+    HttpClient client;
 
     @Test
     public void testHello() throws Exception {
-        HttpRequest request = HttpRequest.GET("/"); 
+        HttpRequest request = HttpRequest.GET("/");
         String body = client.toBlocking().retrieve(request);
-        assertNotNull(body);
-        assertEquals(
-                body,
-                "Micronaut on App Engine"
+        Assertions.assertNotNull(body);
+        Assertions.assertEquals(
+                "Micronaut on App Engine",
+                body
         );
     }
 }
